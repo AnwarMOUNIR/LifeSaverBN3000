43986ef1be439c331c1dce400fbe28b11db2f893
ihsaneabdou07 <ihssaneabdou07@gmail.com>
2026-03-10 15:24:01 +0000

    Update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -17,7 +17,17 @@ def process_data(input_path, output_filename="processed_data.csv"):
     print(f"✔ Success! Processed data saved to: {output_path}")
     
     return output_path
-
+def load_processed_data(file_name="processed_data.csv"):
+    """
+    Loads the processed dataset from data/processed/.
+    """
+    path = os.path.join("data", "processed", file_name)
+    
+    if os.path.exists(path):
+        return pd.read_csv(path)
+    else:
+        print(f"Error: {path} not found.")
+        return None
 if __name__ == "__main__":
     # Run this to test it
     process_data("data/ObesityDataSet_raw_and_data_sinthetic.csv")
