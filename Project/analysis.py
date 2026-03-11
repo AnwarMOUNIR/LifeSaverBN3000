b72e476eedc9e671135c756d12935e7725b8b95f
WissuMissu <wissal.aa05@gmail.com>
2026-03-11 14:53:51 +0000

    Bar charts

--- Project/analysis.py
+++ Project/analysis.py
@@ -2,20 +2,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# 10.1 Load dataset
 df = pd.read_csv("data/ObesityDataSet_raw_and_data_sinthetic.csv")
 
+# 10.2 Basic info
 df.info()
 print(df.describe())
+
+# 11 Missing values
 print(df.isnull().sum())
 
-# handle missing values
+# 12 Handle missing values
 df = df.dropna()
 
+# 13: Histograms
 df.hist(figsize=(12,10))
 plt.show()
 
-# bar charts for categorical variables
-
+# 14 Bar charts
 df['Gender'].value_counts().plot(kind='bar')
 plt.title("Gender Distribution")
 plt.show()
