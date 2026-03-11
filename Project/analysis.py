73c9e7018b9ab41e4439adcae707a84c281cad29
WissuMissu <wissal.aa05@gmail.com>
2026-03-11 14:43:57 +0000

    Histograms for numerical action

--- Project/analysis.py
+++ Project/analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv("data/ObesityDataSet_raw_and_data_sinthetic.csv")
 
@@ -8,4 +10,16 @@ print(df.isnull().sum())
 
 # handle missing values
 df = df.dropna()
-salam
+
+df.hist(figsize=(12,10))
+plt.show()
+
+# bar charts for categorical variables
+
+df['Gender'].value_counts().plot(kind='bar')
+plt.title("Gender Distribution")
+plt.show()
+
+df['family_history_with_overweight'].value_counts().plot(kind='bar')
+plt.title("Family History of Overweight")
+plt.show()
